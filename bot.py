d1f0dae0fa2bd9deb02854ca4e987b5b636b0e7b
¨Gamebreack¨ <victorlescan@gmail.com>
2023-03-07 17:03:35 -0300

    separate files for some variables and privateKeys

--- bot.py
+++ bot.py
@@ -1,37 +1,16 @@
+import libLoja
+import variables
+import privateKeys
 import discord
 from discord.ext import commands
 import random
-import libLoja
 from tabulate import tabulate
 tabulate.PRESERVE_WHITESPACE = True
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-description = '''Eu sou o Edymilson, o bot das crônicas de Módeg.
-Eu posso ser um pouco sarcástico, mas sempre ajudarei quando precisar.'''
-bot = commands.Bot(command_prefix='ed/', intents=intents, description=description)
-textoDeAjuda = '''
-
-Eu sou o Edymilson, o bot das crônicas de Módeg.
-Eu posso ser um pouco sarcástico, mas sempre ajudarei quando precisar.
-Você pode me pedir ajuda usando prefixo (ed/) e me informando o comando que deseja que eu execute.
-
-
-
-* rolar NdN
-Rola um determinado número de dados. Exemplo de uso: ed/rolar 1d6
-
-* Escolha opção 1, opção 2, opção n
-Permite aos jogadores deixar uma escolha a cargo do Edymilson. Exemplo de uso: ed/escolha matar perdoar
-
-* ping
-Mostra a latência do Bot com o servidor. Exemplo de uso: ed/ping
-
-* produtos
-Mostra o estoque de produtos de uma loja. Exemplo de uso: ed/produtos
-
-'''
+bot = commands.Bot(command_prefix='ed/', intents=intents, description=variables.description)
 
 @bot.event
 async def on_ready():
@@ -42,7 +21,7 @@ async def on_ready():
 
 @bot.command()
 async def ajuda(ctx):
-    await ctx.send(textoDeAjuda)
+    await ctx.send(variables.textoDeAjuda)
 
 @bot.command()
 async def rolar(ctx, dice):
@@ -63,4 +42,4 @@ async def escolha(ctx, *choices: str):
 async def loja(ctx):
     await ctx.send(tabulate(libLoja.samplingLoja(), headers=['Item','Quantidade','Preço'], tablefmt='plain', stralign='right'))
 
-bot.run('NzExMzE2MzkyMDE1NTYwNzYz.XsBPLQ.DOChTB_Mfn48ACSlB8FsnTEntb0')
+bot.run(privateKeys.discordToken)
---
+++ variables.py
@@ -0,0 +1,25 @@
+
+description = '''Eu sou o Edymilson, o bot das crônicas de Módeg.
+Eu posso ser um pouco sarcástico, mas sempre ajudarei quando precisar.'''
+textoDeAjuda = '''
+
+Eu sou o Edymilson, o bot das crônicas de Módeg.
+Eu posso ser um pouco sarcástico, mas sempre ajudarei quando precisar.
+Você pode me pedir ajuda usando prefixo (ed/) e me informando o comando que deseja que eu execute.
+
+
+
+* rolar NdN
+Rola um determinado número de dados. Exemplo de uso: ed/rolar 1d6
+
+* Escolha opção 1, opção 2, opção n
+Permite aos jogadores deixar uma escolha a cargo do Edymilson. Exemplo de uso: ed/escolha matar perdoar
+
+* ping
+Mostra a latência do Bot com o servidor. Exemplo de uso: ed/ping
+
+* produtos
+Mostra o estoque de produtos de uma loja. Exemplo de uso: ed/produtos
+
+'''
+
